dd5c26a5328f700a56b2f3e614bfdd01d184dced
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-10-15 00:13:24 +0530

    formatting timestamp

--- public/js/chat.js
+++ public/js/chat.js
@@ -14,7 +14,8 @@ const locationMessageTemplate = document.querySelector('#location-message-templa
 socket.on('message', (message) => {
     console.log(message)
     const html = Mustache.render(messageTemplate, {
-        message
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
