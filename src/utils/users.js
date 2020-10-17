c3089eac4c530ad1b5cdb153985eb4c6fd433264
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-10-18 00:18:14 +0530

    add user

---
+++ src/utils/users.js
@@ -0,0 +1,41 @@
+const users = []
+
+// addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, username, room }) => {
+    // Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    // Check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    // Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    // Store user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+addUser({
+    id: 22,
+    username: 'Andrew  ',
+    room: '  South Philly'
+})
+
+console.log(users)
