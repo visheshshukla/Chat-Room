eae83e791bbd5d52426967c387c2741037b9ff8c
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-10-22 08:54:45 +0530

    updated admin message

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ io.on('connection', (socket) => {
 
         socket.join(user.room)
 
-        socket.emit('message', generateMessage('Admin', 'Welcome!'))
+        socket.emit('message', generateMessage('Admin', 'Welcome! Your Chat Room Is Ready. Happy Chatting. :-)'))
         socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
         io.to(user.room).emit('roomData', {
             room: user.room,
